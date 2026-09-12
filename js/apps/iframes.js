7e5fd87426ba6cf472009d56a630f27ccad4c004
robloxstudio4r <textaway2@outlook.com>
2026-09-11 18:03:50 -0700

    Add makeIframeApp function to create iframe applications

--- js/apps/iframes.js
+++ js/apps/iframes.js
@@ -1,1 +1,7 @@
-
+function makeIframeApp(appId, title, icon, url){
+  return makeWindow(appId, title, icon,
+    '<div style="flex:1;display:flex;flex-direction:column;">'
+    + '<iframe src="' + url + '" style="flex:1;border:none;border-radius:6px;background:white;" sandbox="allow-scripts allow-forms allow-same-origin allow-popups"></iframe>'
+    + '<div style="margin-top:8px;font-size:11px;color:#666;">If not loading, <a href="' + url + '" target="_blank" style="color:#4dabf7;">open in new tab</a>.</div>'
+    + '</div>', 820, 620);
+}
