d05d7c6af3115d4c62b7411e2eecd02fcf533581
robloxstudio4r <textaway2@outlook.com>
2026-09-11 17:52:37 -0700

    Initialize state object with default properties

--- js/state.js
+++ js/state.js
@@ -1,1 +1,9 @@
-
+var ST = {
+  windows: [], z: 100, counter: 0, bootTime: Date.now(),
+  wifiOn: LS.getItem('oc_wifi') === 'true',
+  btOn: LS.getItem('oc_bt') === 'true',
+  btDevice: null,
+  screensaverActive: false,
+  idleTimeout: 60000,
+  lastActivity: Date.now()
+};
